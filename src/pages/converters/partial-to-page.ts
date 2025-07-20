fe9b61a5b2ade559af80f0c70e7322fafc0dbd12
Sergei Kovalenko <smith404@live.com>
2025-07-20 19:27:09 +0200

    index.html as default

--- src/pages/converters/partial-to-page.ts
+++ src/pages/converters/partial-to-page.ts
@@ -1,11 +1,13 @@
 import type { TPage } from "../types/page.js";
 import type { TPartialPage } from "../types/partial-page.js";
+import { convertStringToFilename } from "./string-to-filename.js";
 
 export const convertPartialToPage = (partial: TPartialPage): TPage => {
   return {
     ...partial,
     excludeGlobalApp: partial.excludeGlobalApp ?? false,
     excludeGlobalStylesheet: partial.excludeGlobalStylesheet ?? false,
+    filename: partial.filename ?? convertStringToFilename("index.html"),
     isPublic: partial.isPublic ?? true,
   };
 };
