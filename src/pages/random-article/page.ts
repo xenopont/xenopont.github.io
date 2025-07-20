2185e5c51e4d4ab4450af0d0343053720879c121
Sergei Kovalenko <smith404@live.com>
2025-07-20 23:32:36 +0200

    random article to test article paths

---
+++ src/pages/random-article/page.ts
@@ -0,0 +1,7 @@
+import { convertStringToPath } from "../../converters/string-to-path.js";
+import type { TPartialPage } from "../../types/partial-page.js";
+
+export const randomArticle: TPartialPage = {
+  path: convertStringToPath("random-article"),
+  title: "Random Article",
+};
