8186a7f268c3675b27227b35da6912fabab2d7a2
Sergei Kovalenko <smith404@live.com>
2025-07-29 14:01:14 +0200

    void element markup, container element markup

--- src/render/markup.ts
+++ src/render/markup.ts
@@ -1,9 +1,64 @@
-type TElementAttributes = Record<string, string>;
+/**
+ * https://html.spec.whatwg.org/multipage/syntax.html#elements-2
+ */
 
-const htmlElement = (
-  elementName: string,
-  attributes: TElementAttributes,
-  children: string[],
-): string => {
-  return ``;
+declare const __brandTHtmlElementMarkup: unique symbol;
+type THtmlElementMarkup = string & {
+  [__brandTHtmlElementMarkup]: "THtmlElementMarkup";
+};
+
+type TVoidElementTagName =
+  | "area"
+  | "base"
+  | "br"
+  | "col"
+  | "embed"
+  | "hr"
+  | "img"
+  | "input"
+  | "link"
+  | "meta"
+  | "source"
+  | "track"
+  | "wbr";
+
+// @todo split the type to global, aria-* and data-* attributes
+// @todo limit the type by the allowed for exact element ones
+type THtmlElementAttributes = Record<string, string>;
+
+type TVoidHtmlElementCreateParams = {
+  tagName: TVoidElementTagName;
+  attributes: THtmlElementAttributes;
+};
+const voidHtmlElement = ({
+  tagName,
+  attributes,
+}: TVoidHtmlElementCreateParams): THtmlElementMarkup => {
+  const parts: string[] = [];
+  parts.push(`<${tagName}`);
+  if (Object.keys(attributes).length > 0) {
+    parts.push(
+      ` ${Object.keys(attributes)
+        .map((k) => `${k}="${attributes[k]}"`)
+        .join(" ")} `,
+    );
+  }
+  parts.push(">");
+
+  return parts.join() as THtmlElementMarkup;
+};
+
+type TContainerHtmlElementCreateParams = {
+  tagName: string;
+  attributes: THtmlElementAttributes;
+  children: string[];
+};
+const containerHtmlElement = ({
+  tagName,
+  attributes,
+  children,
+}: TContainerHtmlElementCreateParams): string => {
+  return `<${tagName} ${Object.keys(attributes)
+    .map((k) => `${k}="${attributes[k]}"`)
+    .join(" ")}>${children.join()}</${tagName}>`;
 };
