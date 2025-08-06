db4e010d504452050ade1acb092eef9d081a1874
Sergei Kovalenko <sergei.kovalenko@wunderflats.com>
2025-08-06 17:07:25 +0200

    Detect IP Article

---
+++ src/pages/detect-client-ip/page.ts
@@ -0,0 +1,8 @@
+import { stringToPath } from "../../converters/string-to-path.js";
+import type { TPartialPage } from "../../types/partial-page.js";
+
+export const HowToDetectClientIpPage: TPartialPage = {
+  content: "",
+  path: stringToPath("how-to-get-the-client-ip-address-in-your-app"),
+  title: "How To Get The Client IP Address In Your App",
+};
