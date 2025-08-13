fef04919b994013a380d2fb790f260085fa6ea56
Sergi Kovalenko <smith404@live.com>
2025-08-13 19:05:37 +0200

    publish the first article

---
+++ assets-e20eilr7r4t/js/1l5fcbv4a86.js
@@ -0,0 +1,2 @@
+"use strict";(()=>{document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("blog-section");e&&(e.scrollTop=e.scrollHeight)});})();
+//# sourceMappingURL=1l5fcbv4a86.js.map
---
+++ assets-e20eilr7r4t/js/global.js
@@ -0,0 +1,2 @@
+"use strict";(()=>{var o={error:console.error,log:console.log,warn:console.warn};o.log("Global App Started");})();
+//# sourceMappingURL=global.js.map
