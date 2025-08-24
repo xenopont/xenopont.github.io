95642c542d7989fb5d1a229b3fe383c4af7b6ac8
Sergi Kovalenko <smith404@live.com>
2025-08-24 15:53:12 +0200

    og:tags

--- assets-fseb2gl9hbu/js/nwgafy8cgtp.js
+++ assets-fseb2gl9hbu/js/nwgafy8cgtp.js
@@ -1,2 +1,2 @@
 "use strict";(()=>{document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("blog-section");e&&(e.scrollTop=e.scrollHeight)});})();
-//# sourceMappingURL=argqm4y4y8.js.map
+//# sourceMappingURL=nwgafy8cgtp.js.map
