31a252de9352b1519a2ed9536932217a9cce92e3
Sergi Kovalenko <smith404@live.com>
2025-08-24 15:42:17 +0200

    the image itself

--- src/pages/detect-client-ip/image-1.jpg.ts
+++ src/pages/detect-client-ip/image-1.jpg.ts
@@ -2,6 +2,6 @@ import { filenameToImageUri } from "../../converters/filename-to-image-uri.js";
 import type { TImageUri } from "../../types/image-uri.js";
 
 export const socialCardImageUri: TImageUri = filenameToImageUri(
-  `${import.meta.dirname}/image-1.jpg`,
+  `${import.meta.dirname}/social-network-cover.jpg`,
   "detect-client-ip",
 );
