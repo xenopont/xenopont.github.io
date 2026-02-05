c6119e82682ea70c680abc5453e5d6141df90e62
Sergi Kovalenko <smith404@live.com>
2026-02-05 19:34:46 +0100

    refresh the dev env

---
+++ src/build.ts
@@ -0,0 +1,5 @@
+import { logger } from "./utils/logger.js";
+
+logger.info("Building...");
+logger.info("Done!");
+logger.info("");
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,12 @@
-export const logger = {
-  error: console.error,
-  log: console.log,
-  warn: console.warn,
+/** biome-ignore-all lint/suspicious/noConsole: ILogger implementation */
+type TDebuggable = string | number | object | undefined | unknown;
+
+interface ILogger {
+  info: (message: string) => void;
+  debug: (...data: TDebuggable[]) => void;
+}
+
+export const logger: ILogger = {
+  info: (message: string) => console.log(message),
+  debug: (...data: TDebuggable[]) => console.log(data),
 };
