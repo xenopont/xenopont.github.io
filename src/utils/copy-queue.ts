5fbad9089d0f6e04b2d16c3f5d71d31bf243876c
Sergi Kovalenko <smith404@live.com>
2026-02-22 19:26:46 +0100

    copy queue check if can read and write

--- src/utils/copy-queue.ts
+++ src/utils/copy-queue.ts
@@ -1,4 +1,4 @@
-import { copyFile } from "node:fs/promises";
+import { access, copyFile, constants as fsConst } from "node:fs/promises";
 import type { TLocalFileName, TPublicFileName } from "../config/paths.js";
 import { logger } from "./logger.js";
 
@@ -6,6 +6,9 @@ type TFileCopyOperation = {
   source: TLocalFileName;
   destination: TPublicFileName;
 };
+
+class SourceFileDoesntExistError extends Error {}
+class DestinationFileAlreadyExistsError extends Error {}
 
 class CopyQueue {
   private queue: Set<TFileCopyOperation> = new Set();
@@ -18,22 +21,53 @@ class CopyQueue {
     const promises: Promise<void>[] = [];
     for (const operation of this.queue) {
       promises.push(
-        copyFile(operation.source, operation.destination)
+        Promise.all([
+          this.checkSourceExists(operation.source),
+          this.checkDestinationDoesntExist(operation.destination),
+        ])
+          .then(() => copyFile(operation.source, operation.destination))
           .then(() => {
             logger.info(
               `✅ Copied ${operation.source} to ${operation.destination}`,
             );
           })
-          .catch((error) => {
+          .catch((error: unknown) => {
             logger.error(
               `❌ Cannot copy ${operation.source} to ${operation.destination}`,
             );
-            logger.error(error);
+            if (error instanceof SourceFileDoesntExistError) {
+              logger.info(`Source file ${operation.source} does not exist`);
+              return;
+            }
+            if (error instanceof DestinationFileAlreadyExistsError) {
+              logger.info(
+                `Destination file ${operation.destination} already exists`,
+              );
+              return;
+            }
           }),
       );
     }
 
     await Promise.all(promises);
+  }
+
+  private async checkSourceExists(source: TLocalFileName): Promise<void> {
+    return access(source, fsConst.R_OK).catch(() => {
+      throw new SourceFileDoesntExistError(`Cannot read file ${source}`);
+    });
+  }
+
+  private async checkDestinationDoesntExist(
+    destination: TPublicFileName,
+  ): Promise<void> {
+    return access(destination, fsConst.R_OK)
+      .then(() => {
+        throw new DestinationFileAlreadyExistsError(
+          `File ${destination} already exists`,
+        );
+      })
+      .catch();
   }
 }
 
