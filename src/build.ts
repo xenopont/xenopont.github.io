3b5a8cbaa80dc95fcde4a5bd89aee139e425bc50
Sergi Kovalenko <smith404@live.com>
2026-02-26 21:40:37 +0100

    attaching templates

--- src/build.ts
+++ src/build.ts
@@ -1,4 +1,5 @@
 import { content } from "./content/all.js";
+import { getTemplate } from "./templates/active.js";
 import { cleanDist } from "./utils/clean-dist.js";
 import { copyQueue } from "./utils/copy-queue.js";
 import { logger } from "./utils/logger.js";
@@ -12,7 +13,9 @@ const main = async (): Promise<void> => {
   logger.debug(`Content found: ${content.length} items`);
   for (const page of content) {
     logger.debug(`Rendering ${page.title}`);
-    logger.debug(page.content.toString());
+    const template = getTemplate(page.template);
+    const renderedHtml = template.render(page);
+    logger.debug(renderedHtml);
   }
 
   // Start the copy queue only after all pages are rendered.
--- src/templates/active.ts
+++ src/templates/active.ts
@@ -1,6 +1,24 @@
+import { defaultTemplate } from "./default.js";
+import type { THtmlTemplate } from "./html-template.js";
+
 export const ACTIVE_TEMPLATES = {
   default: "default",
 } as const;
 
 export type TTemplateId =
   (typeof ACTIVE_TEMPLATES)[keyof typeof ACTIVE_TEMPLATES];
+
+class ETemplateNotFound extends Error {}
+
+const templateRegistry: Map<TTemplateId, THtmlTemplate> = new Map<
+  TTemplateId,
+  THtmlTemplate
+>([[ACTIVE_TEMPLATES.default, defaultTemplate]]);
+
+export function getTemplate(id: TTemplateId): THtmlTemplate {
+  const template = templateRegistry.get(id);
+  if (!template) {
+    throw new ETemplateNotFound(`❌ Template "${id}" not found in registry`);
+  }
+  return template;
+}
