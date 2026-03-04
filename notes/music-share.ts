a93a1812311a4f0fb6bd75aef29858bb64e7d8d1
Sergi Kovalenko <smith404@live.com>
2026-03-04 21:26:58 +0100

    new note

---
+++ notes/music-share.ts
@@ -0,0 +1,6 @@
+/**
+ * an extremely romantic and sad article on how individual music understanding is
+ * and that's why you can't share your impressions of the music with others
+ * even if they understand you
+ * you are 100% lonely
+ */
