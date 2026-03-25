9da7c2303fe22c36de6aaaca3932375d6f0e3ba0
Sergi Kovalenko <smith404@live.com>
2026-03-25 19:57:55 +0100

    home page desktop css

--- src/templates/home/page-header.ts
+++ src/templates/home/page-header.ts
@@ -1,8 +1,10 @@
 import { domainDisplayName } from "../../config/constants.js";
-import { h1, header, safe, section } from "../../html/elements.js";
+import { div, h1, header, safe, section } from "../../html/elements.js";
 import type { THtmlEntity } from "../../html/entities.js";
 
 export const pageHeader: THtmlEntity = header({ id: "home-page-header" }, [
-  section({ id: "title-section" }, [h1({}, [safe(domainDisplayName)])]),
+  section({ id: "title-section" }, [
+    div({ id: "title-wrapper" }, [h1({}, [safe(domainDisplayName)])]),
+  ]),
   section({ id: "header-space" }, []),
 ]);
