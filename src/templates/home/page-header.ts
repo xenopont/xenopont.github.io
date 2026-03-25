6c13a3d768c832bc59847a5632377aafe3ede968
Sergi Kovalenko <smith404@live.com>
2026-03-25 18:26:48 +0100

    space to brief

--- src/templates/home/page-header.ts
+++ src/templates/home/page-header.ts
@@ -1,7 +1,8 @@
 import { domainDisplayName } from "../../config/constants.js";
-import { h1, header, safe } from "../../html/elements.js";
+import { h1, header, safe, section } from "../../html/elements.js";
 import type { THtmlEntity } from "../../html/entities.js";
 
 export const pageHeader: THtmlEntity = header({ id: "home-page-header" }, [
-  h1({}, [safe(domainDisplayName)]),
+  section({ id: "title-section" }, [h1({}, [safe(domainDisplayName)])]),
+  section({ id: "header-space" }, []),
 ]);
