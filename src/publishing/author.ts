4d2f56584f7e8afd574556f470d87fc4afbd2460
Sergei Kovalenko <sergei.kovalenko@wunderflats.com>
2026-03-30 17:00:27 +0200

    broken author

---
+++ src/publishing/author.ts
@@ -0,0 +1,6 @@
+import type { TWebUri } from "../utils/paths.js";
+
+export interface IAuthor {
+  name: string;
+  url: TWebUri;
+}
--- src/publishing/publishable.ts
+++ src/publishing/publishable.ts
@@ -3,11 +3,7 @@ import type { THtmlEntity, TSafeText } from "../html/entities.js";
 import type { TTemplateId } from "../templates/active.js";
 import type { TValidHtmlFileName } from "../utils/filenames.js";
 import type { TPublicDirectory, TWebUri } from "../utils/paths.js";
-
-interface IAuthor {
-  name: string;
-  url: string; // todo make valid local or external URL/TWebUri
-}
+import type { IAuthor } from "./author.js";
 
 export interface TPageColors {
   accent: string;
